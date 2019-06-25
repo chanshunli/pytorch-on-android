174b95a9ef08d2b160904b32b8a1b537c8a68907
Steve Chan <chanshunli@gmail.com>
2019-06-26 00:02:59 +0800

    坚持到最后一刻

--- setuppytorch.sh
+++ setuppytorch.sh
@@ -2,6 +2,7 @@
 pacman -S base-devel
 pacman -S git
 pacman -S python-pip
+pacman -S cmake
 
 # https://docs.anaconda.com/anaconda/install/linux
 pacman -Sy libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl  alsa-lib libglvnd
@@ -14,6 +15,6 @@ cd pytorch
 git submodule update --init --recursive
 pip install -U setuptools
 pip install -r requirements.txt
-python setup.py build_deps install
+python setup.py build_deps install # python setup.py build_deps install
 cd ..
 
